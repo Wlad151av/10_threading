02bd1c6e659dc81346d4d02992d233ab6a77a0f1
Wlad151av <vvlads36@gmail.com>
2024-11-01 11:15:25 +0300

    Add files via upload

--- module_10_2.py
+++ module_10_2.py
@@ -4,25 +4,24 @@ import time
 
 class Knight(threading.Thread):
     def __init__(self, name, power):
-        threading.Thread.__init__(self)
+        super().__init__()
         self.name = name
         self.power = power
         self.enemy_num = 100
-        self.days = 0
-
-
+        
     def run(self):
 
         print(f'{self.name}, на нас напали!')
-        while self.enemy_num > 0:
-            self.days += 1
+        days = 0
+        while self.enemy_num >= self.power:
             time.sleep(1)
             self.enemy_num -= self.power
-            print(f'{self.name} сражается {self.days} день(дня, дней)..., осталось {self.enemy_num} воинов.')
-        print(f'{self.name} одержал победу спустя {self.days} дней(дня)!')
+            days += 1
+            print(f'{self.name} сражается {days} день(дня, дней)..., осталось {self.enemy_num} воинов.')
+        print(f'{self.name} одержал победу спустя {days} дней(дня)!')
 
 
-first_knight = Knight('Sir Lancelot', 10)
+first_knight = Knight('Sir Lancelot', 11)
 second_knight = Knight("Sir Galahad", 20)
 first_knight.start()
 second_knight.start()
